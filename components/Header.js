25070cec601f7446e0581671a6b599d28ad3f70e
taurusilver7 <prasad20697@gmail.com>
2021-05-16 10:11:48 +0530

    image element added

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,16 @@ const Header = () => {
     <header>
       {/* left */}
       <div>
-        <Image />
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div>
+          <input type="text" placeholder="Search Facebook" />
+        </div>
       </div>
-
       {/* center */}
 
       {/* right */}
