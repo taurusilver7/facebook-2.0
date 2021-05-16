4043446fd0eab824f9e9d1a633849f8bc7f57fe8
taurusilver7 <prasad20697@gmail.com>
2021-05-16 18:54:37 +0530

    image added to header

--- components/Header.js
+++ components/Header.js
@@ -1,20 +1,36 @@
 import Image from "next/image";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
     <header>
       {/* left */}
-      <div>
+      <div className="flex item-center">
         <Image
           src="https://links.papareact.com/5me"
           width={40}
           height={40}
           layout="fixed"
         />
-        <div>
+        <div className="flex ml-2 items-centre rounded-full">
+          <SearchIcon className="h-6" />
           <input type="text" placeholder="Search Facebook" />
         </div>
       </div>
+
       {/* center */}
 
       {/* right */}
