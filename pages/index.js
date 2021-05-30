ab7006b6d7b8fac8f0e1eceac3304b162a397021
taurusilver7 <prasad20697@gmail.com>
2021-05-30 22:22:46 +0530

    page re-adjusted

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { getSession } from "next-auth/client";
 import Head from "next/head";
 import Header from "../components/Header";
+import Login from "../components/Login";
 
-export default function Home() {
+export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
     <div>
       <Head>
@@ -13,4 +16,15 @@ export default function Home() {
       <Header />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // get the user from server
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
